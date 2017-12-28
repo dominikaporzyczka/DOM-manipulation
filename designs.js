3c8522d1b3cbc1a6fbb38bf3cb6adfe7e7bebb27
Dominika Porzyczka <dominikaporzyczka@gmail.com>
2017-12-28 18:32:30 +0100

    Improvements

--- designs.js
+++ designs.js
@@ -3,7 +3,7 @@ $( document ).ready(function() {
     $('#task-form')
         .submit( function(event) {
             var inputVal = $('input[name=newTask]').val();
-            $('#list').append('<li><span class="task">' + inputVal + '</span><button class="delete-btn btn">x</button></li>');
+            $('#list').append('<li><button class="delete-btn btn">x</button><span class="task">' + inputVal + '</span></li>');
             $(this).trigger('reset');
 
             event.preventDefault();
