65ab360471357a9e37708d8a6ddf8874dcf4b52b
Dominika Porzyczka <dominikaporzyczka@gmail.com>
2017-12-28 18:22:06 +0100

    Improve remove method, added method for done task

--- designs.js
+++ designs.js
@@ -3,14 +3,21 @@ $( document ).ready(function() {
     $('#task-form')
         .submit( function(event) {
             var inputVal = $('input[name=newTask]').val();
-            $('#list').append('<li><span>' + inputVal + '</span><button class="delete-btn btn">x</button></li>');
+            $('#list').append('<li><span class="task">' + inputVal + '</span><button class="delete-btn btn">x</button></li>');
             $(this).trigger('reset');
 
-        $('.delete-btn').click( function() {
-            $(this).parents('li').remove();
+            event.preventDefault();
         });
+    
+        $('#list')
+            .on('click', '.delete-btn', function() {
+                $(this).parents('li').remove();
+            })
             
-        event.preventDefault();
-        });
+            .on('click', '.task', function() {
+                $(this).toggleClass('task-done');
+            });
+
+       
 
 });
