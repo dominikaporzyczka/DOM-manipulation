052e85b70601376c46e8d242df38529e1adc78c2
Dominika Porzyczka <dominikaporzyczka@gmail.com>
2017-12-28 17:02:10 +0100

    Added function to remove task, added more styles

--- designs.js
+++ designs.js
@@ -3,9 +3,13 @@ $( document ).ready(function() {
     $('#task-form')
         .submit( function(event) {
             var inputVal = $('input[name=newTask]').val();
-            $('#list').append('<li>' + inputVal + '</li>');
+            $('#list').append('<li><span>' + inputVal + '</span><button class="delete-btn btn">x</button></li>');
             $(this).trigger('reset');
 
+        $('.delete-btn').click( function() {
+            $(this).parents('li').remove();
+        });
+            
         event.preventDefault();
         });
 
